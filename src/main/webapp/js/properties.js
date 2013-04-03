58a1bca0c0c17e6773dda48c9d67a66ec58ce9cd
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-04-03 08:51:17 +0000

    Alignment with the new AC service API

--- src/main/webapp/js/properties.js
+++ src/main/webapp/js/properties.js
@@ -1,23 +1,8 @@
-/**
- * Copyright 2012-2013 Trento RISE
- * 
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- * 
- *        http://www.apache.org/licenses/LICENSE-2.0
- * 
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 function Properties() {
 	//this.staticUrl = '/portfolio/';
 	this.staticUrl = '/mycvs/';
-	this.baseUrl = 'https://' + window.document.location.hostname + ':' + window.document.location.port + this.staticUrl;
-	this.authUrl = 'https://ac.smartcampuslab.it/ac-service-provider-web/ac/getToken?code=&redirect=';
+	this.baseUrl = 'https://' + window.document.location.hostname + (window.document.location.port==''?'':(':'+window.document.location.port)) + this.staticUrl;
+	this.authUrl = 'https://ac.smartcampuslab.it/accesstoken-provider/ac/getToken?code=&redirect=';
 	this.validationUrl = this.baseUrl + 'rest/validatetoken/';
 
 	this.showmsec = 0;
--- src/main/webapp/js/sec.js
+++ src/main/webapp/js/sec.js
@@ -1,18 +1,3 @@
-/**
- * Copyright 2012-2013 Trento RISE
- * 
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- * 
- *        http://www.apache.org/licenses/LICENSE-2.0
- * 
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 function Security() {
 };
 
@@ -50,11 +35,12 @@ Security.prototype.checkLogin = function() {
 	} else if (document.location.hash != null && document.location.hash.length > 1) {
 		var code = document.location.hash.substr(1);
 		$.ajax({
+			dataType: "xml",
 			sync : false,
 			type : 'POST',
 			url : __Properties.validationUrl+code,
 			success : function(data, textStatus, jqXHR) {
-				var token = data.substr(1,data.length-2);
+				var token = $(data).find("token").text();
 				document.cookie = 'auth_token=' + token;
 		 		document.location = __Properties.baseUrl;
 			},
