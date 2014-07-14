83fa8bf35d7f649f3cb25729a9d4a33641ad5a84
mirkoperillo <mirko.perillo@gmail.com>
2014-07-14 14:04:20 +0200

    refactor and added userproduceddata

--- src/main/java/eu/trentorise/smartcampus/portfolio/dataexporter/DataRetriever.java
+++ src/main/java/eu/trentorise/smartcampus/portfolio/dataexporter/DataRetriever.java
@@ -29,6 +29,7 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 
 import eu.trentorise.smartcampus.portfolio.models.Portfolio;
+import eu.trentorise.smartcampus.portfolio.models.UserProducedData;
 import eu.trentorise.smartcampus.portfolio.processor.EventProcessorImpl;
 import eu.trentorise.smartcampus.presentation.common.exception.DataException;
 import eu.trentorise.smartcampus.presentation.common.exception.NotFoundException;
@@ -54,11 +55,45 @@ public class DataRetriever {
 	private static final String DB_USER = "ac";
 	private static final String DB_PWD = "ac";
 
+	private static final boolean CONVERT_PORTFOLIO = false;
 	private static final boolean PRINT_PORTFOLIO = false;
-	private static final boolean OVERWRITE_IF_EXIST = false;
+	private static final boolean PORTFOLIO_OVERWRITE_IF_EXIST = false;
 
-	public static void main(String[] args) {
-		MongoOperations mongo = null;
+	private static final boolean CONVERT_USERDATA = true;
+	private static final boolean PRINT_USERDATA = false;
+	private static final boolean USERDATA_OVERWRITE_IF_EXIST = false;
+
+	private static MongoOperations mongo;
+	private static BasicObjectSyncMongoStorage storage;
+	private static EventProcessorImpl eventProcessor;
+	private static JMSDomainEngineClient domainClient;
+	private static ObjectMapper mapper;
+
+	private static void init() {
+		System.out.println(String.format("BROKER URL: %s", BROKER_URL));
+		System.out.println(String.format("BROKER CLIENT ID: %s",
+				BROKER_CLIENT_ID));
+		System.out.println(String.format("MONGO HOST: %s", MONGO_HOST));
+		System.out.println(String.format("MONGO PORT: %s", MONGO_PORT));
+		System.out.println(String.format("MONGO NAME: %s", MONGO_NAME));
+
+		System.out.println(String.format("USER DB URL: %s", DB_URL));
+		System.out.println(String.format("USER DB USERNAME: %s", DB_USER));
+		System.out.println(String.format("USER DB PWD: %s", DB_PWD));
+
+		System.out.println(String
+				.format("PRINT PORTFOLIO: %s", PRINT_PORTFOLIO));
+		System.out.println(String.format("OVERWRITE IF PORTFOLIO EXIST: %s",
+				PORTFOLIO_OVERWRITE_IF_EXIST));
+		System.out.println(String.format("CONVERT PORTFOLIO: %s",
+				CONVERT_PORTFOLIO));
+
+		System.out.println(String.format("PRINT USERDATA: %s", PRINT_USERDATA));
+		System.out.println(String.format("OVERWRITE IF USERDATA EXIST: %s",
+				USERDATA_OVERWRITE_IF_EXIST));
+		System.out.println(String.format("CONVERT USERDATA: %s",
+				CONVERT_USERDATA));
+
 		try {
 			mongo = new MongoTemplate(new SimpleMongoDbFactory(new Mongo(
 					MONGO_HOST, MONGO_PORT), MONGO_NAME));
@@ -69,29 +104,31 @@ public class DataRetriever {
 			exitMsg("Exception connecting to mongo db " + MONGO_NAME + ": "
 					+ e.getMessage());
 		}
-		BasicObjectSyncMongoStorage storage = new BasicObjectSyncMongoStorage(
-				mongo);
 
-		EventProcessorImpl eventProcessor = new EventProcessorImpl();
+		storage = new BasicObjectSyncMongoStorage(mongo);
+		System.out.println(String.format("Mongo storage initialized"));
+
+		eventProcessor = new EventProcessorImpl();
 		eventProcessor.setStorage(storage);
-		JMSDomainEngineClient domainClient = null;
+		System.out.println(String.format("Event processor initialized"));
 		try {
 			domainClient = new JMSDomainEngineClient(
 					new ActiveMQConnectionFactory(BROKER_URL));
 			domainClient.setClientId(BROKER_CLIENT_ID);
 			domainClient.setDomainUpdateListener(eventProcessor);
+			System.out
+					.println(String.format("DomainObject client initialized"));
 		} catch (InvocationException e) {
 			exitMsg("Exception connecting to domainClient " + e.getMessage());
 		}
 
-		List<Long> userIds = new ArrayList<Long>();
-		int totalPortfolio = 0;
-		int convertedPortfolio = 0;
-
-		ObjectMapper mapper = new ObjectMapper();
+		mapper = new ObjectMapper();
 		mapper.configure(Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		System.out.println(String.format("Jackson converter initialized"));
+	}
 
-		// retrieve all system uses
+	private static List<Long> getUsers() {
+		List<Long> userIds = new ArrayList<Long>();
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			Connection conn = DriverManager.getConnection(DB_URL, DB_USER,
@@ -111,104 +148,209 @@ public class DataRetriever {
 		} catch (SQLException e) {
 			exitMsg("Exception connecting ac db " + e.getMessage());
 		}
+
+		return userIds;
+	}
+
+	public static void main(String[] args) {
+		init();
+
+		// retrieve all system uses
+		List<Long> userIds = getUsers();
+
 		System.out.println(String.format("Founded %s users", userIds.size()));
+
 		if (userIds.size() == 0) {
 			System.exit(0);
 		} else {
-			for (Long uid : userIds) {
-				// retrieve all portfolio of a user
-				Map<String, Object> pars = new TreeMap<String, Object>();
-				pars.put("userId", uid.toString());
-				pars.put("deleted", false);
-				try {
-					List<String> res = domainClient.searchDomainObjects(
-							"smartcampus.services.esse3.Portfolio", pars,
-							"vas_portfolio_subscriber");
-					totalPortfolio += res.size();
+			if (CONVERT_PORTFOLIO) {
+				convertPortfolio(userIds);
+			}
+			if (CONVERT_USERDATA) {
+				convertUserData(userIds);
+			}
+		}
 
-					for (String p : res) {
-						JSONObject jsonObj;
+		System.exit(0);
+	}
+
+	private static void convertPortfolio(List<Long> userIds) {
+		int totalPortfolio = 0, convertedPortfolio = 0;
+		for (Long uid : userIds) {
+			// retrieve all portfolio of a user
+			Map<String, Object> pars = new TreeMap<String, Object>();
+			pars.put("userId", uid.toString());
+			pars.put("deleted", false);
+			try {
+				List<String> res = domainClient.searchDomainObjects(
+						"smartcampus.services.esse3.Portfolio", pars,
+						"vas_portfolio_subscriber");
+				totalPortfolio += res.size();
+
+				for (String p : res) {
+					JSONObject jsonObj;
+					try {
+						jsonObj = new JSONObject(p);
+						String portfolioContent = jsonObj.getString("content");
+
+						Portfolio portfolio = mapper.readValue(
+								portfolioContent, Portfolio.class);
+						portfolio.setId(jsonObj.getString("id"));
+
 						try {
-							jsonObj = new JSONObject(p);
-							String portfolioContent = jsonObj
-									.getString("content");
-
-							Portfolio portfolio = mapper.readValue(
-									portfolioContent, Portfolio.class);
-							portfolio.setId(jsonObj.getString("id"));
-
-							try {
-								if (!OVERWRITE_IF_EXIST
-										&& storage.getObjectById(portfolio
-												.getId()) != null) {
+							if (!PORTFOLIO_OVERWRITE_IF_EXIST
+									&& storage.getObjectById(portfolio.getId()) != null) {
+								System.out.println(String.format(
+										"Portfolio %s already stored in db",
+										portfolio.getId()));
+							} else {
+								try {
+									storage.storeObject(portfolio);
 									System.out
 											.println(String
-													.format("Portfolio %s already stored in db",
+													.format("Stored on mongo portfolio %s of user %s",
+															portfolio.getId(),
+															uid));
+									convertedPortfolio++;
+								} catch (DataException e) {
+									System.out
+											.println(String
+													.format("Exception storing in mongo portfolio %s",
 															portfolio.getId()));
-								} else {
-									try {
-										storage.storeObject(portfolio);
-										System.out
-												.println(String
-														.format("Stored on mongo portfolio %s of user %s",
-																portfolio
-																		.getId(),
-																uid));
-										convertedPortfolio++;
-									} catch (DataException e) {
-										System.out
-												.println(String
-														.format("Exception storing in mongo portfolio %s",
-																portfolio
-																		.getId()));
-									}
 								}
-							} catch (NotFoundException e) {
-								System.out
-										.println(String
-												.format("Exception searching in mongo portfolio %s",
-														portfolio.getId()));
-								continue;
-							} catch (DataException e) {
-								System.out
-										.println(String
-												.format("Exception searching in mongo portfolio %s",
-														portfolio.getId()));
-								continue;
 							}
-						} catch (JSONException e) {
-							exitMsg(String
-									.format("Exception extracting portfolio content: %s (%s)",
-											p, e.getMessage()));
-						} catch (JsonParseException e) {
-							exitMsg(String
-									.format("Exception extracting portfolio content: %s (%s)",
-											p, e.getMessage()));
-						} catch (JsonMappingException e) {
-							exitMsg(String
-									.format("Exception extracting portfolio content: %s (%s)",
-											p, e.getMessage()));
-						} catch (IOException e) {
-							exitMsg(String
-									.format("Exception extracting portfolio content: %s (%s)",
-											p, e.getMessage()));
+						} catch (NotFoundException e) {
+							System.out
+									.println(String
+											.format("Exception searching in mongo portfolio %s",
+													portfolio.getId()));
+							continue;
+						} catch (DataException e) {
+							System.out
+									.println(String
+											.format("Exception searching in mongo portfolio %s",
+													portfolio.getId()));
+							continue;
 						}
+					} catch (JSONException e) {
+						exitMsg(String
+								.format("Exception extracting portfolio content: %s (%s)",
+										p, e.getMessage()));
+					} catch (JsonParseException e) {
+						exitMsg(String
+								.format("Exception extracting portfolio content: %s (%s)",
+										p, e.getMessage()));
+					} catch (JsonMappingException e) {
+						exitMsg(String
+								.format("Exception extracting portfolio content: %s (%s)",
+										p, e.getMessage()));
+					} catch (IOException e) {
+						exitMsg(String
+								.format("Exception extracting portfolio content: %s (%s)",
+										p, e.getMessage()));
 					}
-					if (PRINT_PORTFOLIO) {
-						for (String r : res) {
-							System.out.println(r);
-						}
-					}
-				} catch (InvocationException e) {
-					exitMsg("Exception searching domain object portfolios of user "
-							+ uid + " " + e.getMessage());
 				}
+				if (PRINT_PORTFOLIO) {
+					for (String r : res) {
+						System.out.println(r);
+					}
+				}
+			} catch (InvocationException e) {
+				exitMsg("Exception searching domain object portfolios of user "
+						+ uid + " " + e.getMessage());
 			}
 		}
 		System.out.println(String.format("Founded %s portfolio, converted %s",
 				totalPortfolio, convertedPortfolio));
-		System.out.println("DONE!");
-		System.exit(0);
+		System.out.println("Portfolio DONE!");
+	}
+
+	private static void convertUserData(List<Long> userIds) {
+		int totalUserdData = 0, convertedUserData = 0;
+		// retrieve all user data of a user
+		for (Long uid : userIds) {
+			Map<String, Object> pars = new TreeMap<String, Object>();
+			pars.put("userId", uid.toString());
+			try {
+				List<String> res = domainClient.searchDomainObjects(
+						"smartcampus.services.esse3.UserProducedData", pars,
+						"vas_portfolio_subscriber");
+				totalUserdData += res.size();
+
+				for (String p : res) {
+					JSONObject jsonObj;
+					try {
+						jsonObj = new JSONObject(p);
+						String userDataContent = jsonObj.getString("content");
+
+						UserProducedData userData = mapper.readValue(
+								userDataContent, UserProducedData.class);
+						userData.setId(jsonObj.getString("id"));
+
+						try {
+							if (!USERDATA_OVERWRITE_IF_EXIST
+									&& storage.getObjectById(userData.getId()) != null) {
+								System.out.println(String.format(
+										"Userdata %s already stored in db",
+										userData.getId()));
+							} else {
+								try {
+									storage.storeObject(userData);
+									System.out
+											.println(String
+													.format("Stored on mongo userdata %s of user %s",
+															userData.getId(),
+															uid));
+									convertedUserData++;
+								} catch (DataException e) {
+									System.out
+											.println(String
+													.format("Exception storing in mongo userdata %s",
+															userData.getId()));
+								}
+							}
+						} catch (NotFoundException e) {
+							System.out.println(String.format(
+									"Exception searching in mongo userdata %s",
+									userData.getId()));
+							continue;
+						} catch (DataException e) {
+							System.out.println(String.format(
+									"Exception searching in mongo userdata %s",
+									userData.getId()));
+							continue;
+						}
+					} catch (JSONException e) {
+						exitMsg(String
+								.format("Exception extracting userdata content: %s (%s)",
+										p, e.getMessage()));
+					} catch (JsonParseException e) {
+						exitMsg(String
+								.format("Exception extracting userdata content: %s (%s)",
+										p, e.getMessage()));
+					} catch (JsonMappingException e) {
+						exitMsg(String
+								.format("Exception extracting userdata content: %s (%s)",
+										p, e.getMessage()));
+					} catch (IOException e) {
+						exitMsg(String
+								.format("Exception extracting userdata content: %s (%s)",
+										p, e.getMessage()));
+					}
+				}
+				if (PRINT_USERDATA) {
+					for (String r : res) {
+						System.out.println(r);
+					}
+				}
+			} catch (InvocationException e) {
+				exitMsg("Exception searching domain object userdata of user "
+						+ uid + " " + e.getMessage());
+			}
+		}
+		System.out.println(String.format("Founded %s userData, converted %s",
+				totalUserdData, convertedUserData));
+		System.out.println("Userdata DONE!");
 	}
 
 	private static void exitMsg(String msg) {
